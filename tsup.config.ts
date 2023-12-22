e9eeaf6df8a71914a3beb468b87228d95282ad89
Simon Green <simon@captaincodeman.com>
2023-12-22 10:11:09 -0700

    bundle uint8array-extras and set firebase peer deps
    also update dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: ['src/lib/index.ts', 'src/lib/firebase/index.ts', 'src/lib/firebase.server/index.ts'],
   format: ['esm'],
   external: ['firebase', 'firebase-admin'],
+  noExternal: ['uint8array-extras'],
   splitting: true,
   sourcemap: false,
   minify: true,
